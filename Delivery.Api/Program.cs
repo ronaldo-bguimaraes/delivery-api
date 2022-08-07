a1cae349e3a024f747326cdd814ac82942e99daf
Ronaldo Guimarães <ronaldobgui@outlook.com>
2022-08-07 18:26:47 -0400

    Configure Migrations

--- Delivery.Api/Program.cs
+++ Delivery.Api/Program.cs
@@ -11,12 +11,14 @@ namespace Delivery.Api
       CreateHostBuilder(args).Build().Run();
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+      return Host.CreateDefaultBuilder(args)
+      .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+      .ConfigureWebHostDefaults(webBuilder =>
+      {
+        webBuilder.UseStartup<Startup>();
+      });
+    }
   }
 }
--- Delivery.Infrastructure/Data/SqlContext.cs
+++ Delivery.Infrastructure/Data/SqlContext.cs
@@ -3,18 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Delivery.Infrastructure.Data
 {
-    // Db context  rastreia as alterações feitas em entidades. Essas entidades rastreadas, por sua vez, 
-    // conduzem as alterações no banco de dados quando Save Changes é chamado. 
-    public class SqlContext : DbContext
-    {
-        public SqlContext() { }
-        
-        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+  // Db context  rastreia as alterações feitas em entidades. Essas entidades rastreadas, por sua vez, 
+  // conduzem as alterações no banco de dados quando Save Changes é chamado. 
+  public class SqlContext : DbContext
+  {
+    public SqlContext() { }
 
-        public DbSet<Cliente> Clientes { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<Endereco> Enderecos { get; set; }
+    public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
 
-  
-    }
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+  }
 }
