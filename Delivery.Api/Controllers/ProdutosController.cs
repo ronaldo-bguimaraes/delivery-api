57ac5f53d4428c34b7ff4ef942cb5872c297a8dc
Ronaldo Guimarães <ronaldobgui@outlook.com>
2022-08-14 04:59:38 -0400

    Use method Save in Produto

---
+++ Delivery.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,114 @@
+﻿using Delivery.Application;
+using Delivery.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Delivery.Api.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class Produtosontroller : Controller
+  {
+    // Fazer um controller para chamar a applicationService
+    private readonly IApplicationServiceUsuario applicationServiceUsuario;
+
+    public Produtosontroller(IApplicationServiceUsuario _applicationServiceUsuario)
+    {
+      applicationServiceUsuario = _applicationServiceUsuario;
+    }
+
+    // GET api/values
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+      return Ok(applicationServiceUsuario.GetAll());
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+      return Ok(applicationServiceUsuario.GetById(id));
+    }
+
+    // POST api/values
+    [HttpPost]
+    [AllowAnonymous]
+    public ActionResult Post([FromBody] UsuarioDto usuarioDto)
+    {
+      try
+      {
+        if (usuarioDto == null)
+        {
+          return NotFound();
+        }
+        applicationServiceUsuario.Save(usuarioDto);
+        return Ok("Usuario Cadastrado com sucesso!");
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public IActionResult Authenticate([FromBody] UsuarioDto usuarioDto)
+    {
+      try
+      {
+        var user = applicationServiceUsuario.Authenticate(usuarioDto);
+
+        if (user == null)
+        {
+          return BadRequest(new { message = "Usuário ou senha inválidos" });
+        }
+
+        return Ok(user);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex);
+      }
+    }
+
+    // PUT api/values/5
+    [HttpPut]
+    public ActionResult Put([FromBody] UsuarioDto usuarioDto)
+    {
+      try
+      {
+        if (usuarioDto == null)
+          return NotFound();
+
+        applicationServiceUsuario.Save(usuarioDto);
+        return Ok("Usuario Atualizado com sucesso!");
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+    // DELETE api/values/5
+    [HttpDelete()]
+    public ActionResult Delete([FromBody] UsuarioDto UsuarioDto)
+    {
+      try
+      {
+        if (UsuarioDto == null)
+        {
+          return NotFound();
+        }
+        applicationServiceUsuario.Remove(UsuarioDto);
+        return Ok("Usuario Removido com sucesso!");
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+  }
+}
