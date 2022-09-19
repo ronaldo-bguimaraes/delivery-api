96951a42d0c6952179a5461a72fa723a30cd87ce
Ronaldo Guimarães <ronaldobgui@outlook.com>
2022-09-18 23:32:47 -0400

    Ajuste conflitos

--- Delivery.Application/Dtos/FormaPagamentoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Delivery.Dtos
-{
-  public class FormaPagamentoDto
-  {
-    public int Id { get; set; }
-    
-    public string Descricao { get; set; }
-  }
-}
--- Delivery.Domain.Core.Services/ServiceFornecedor.cs
+++ Delivery.Domain.Core.Services/ServiceFornecedor.cs
@@ -1,23 +1,23 @@
-﻿using Delivery.Domain.Core.Interfaces.Repositories;
-using Delivery.Domain.Core.Interfaces.Services;
-using Delivery.Domain.Entities;
-using System.Collections.Generic;
+﻿// using Delivery.Domain.Core.Interfaces.Repositories;
+// using Delivery.Domain.Core.Interfaces.Services;
+// using Delivery.Domain.Entities;
+// using System.Collections.Generic;
 
-namespace Delivery.Domain.Core.Services
-{
-  public class ServiceEndereco : ServiceBase<Endereco>, IServiceEndereco
-  {
-    private readonly IRepositoryEndereco repositoryEndereco;
+// namespace Delivery.Domain.Core.Services
+// {
+//   public class ServiceEndereco : ServiceBase<Endereco>, IServiceEndereco
+//   {
+//     private readonly IRepositoryEndereco repositoryEndereco;
 
-    public ServiceEndereco(IRepositoryEndereco _repositoryEndereco) : base(_repositoryEndereco)
-    {
-      repositoryEndereco = _repositoryEndereco;
-    }
+//     public ServiceEndereco(IRepositoryEndereco _repositoryEndereco) : base(_repositoryEndereco)
+//     {
+//       repositoryEndereco = _repositoryEndereco;
+//     }
 
-    // não está funcionando
-    public ICollection<Endereco> GetByUsuarioId(int usuarioId)
-    {
-      return repositoryEndereco.GetByUsuarioId(usuarioId);
-    }
-  }
-}
+//     // não está funcionando
+//     public ICollection<Endereco> GetByUsuarioId(int usuarioId)
+//     {
+//       return repositoryEndereco.GetByUsuarioId(usuarioId);
+//     }
+//   }
+// }
