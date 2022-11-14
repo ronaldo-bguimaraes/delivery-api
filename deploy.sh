091e753fa781a19070b86345106409ca577ceedf
Ronaldo Guimarães <ronaldobgui@outlook.com>
2022-11-14 01:18:59 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-# enter the github repository
-cd ~/delivery-api
+clear_build() {
+  sudo rm -r ./*/bin
+  sudo rm -r ./*/obj
+}
 # ignore local changes and pull
 echo "Updating local repository..."
 git fetch origin main 2>&1> /dev/null
 git reset --hard origin/main 2>&1> /dev/null
 git pull origin main 2>&1> /dev/null
+chmod +x ./deploy.sh
 echo "Deploying commit: $(git log --pretty=format:'%s (%an - %ae)' -1)"
 dotnet format 2>&1> /dev/null
 export ASPNETCORE_ENVIRONMENT=Development
 echo "Updating $ASPNETCORE_ENVIRONMENT environment..."
-# clear trash
-sudo rm -r ~/delivery-api/*/bin
-sudo rm -r ~/delivery-api/*/obj
-su - "$USER" -c "dotnet ef database update --project ~/delivery-api/Delivery.Api" 2>&1> /dev/null
+clear_build
+dotnet ef database update --project ./Delivery.Api 2>&1> /dev/null
 echo "$ASPNETCORE_ENVIRONMENT environment update completed..."
 
 # test
@@ -25,7 +26,7 @@ if [ $result -eq 0 ]; then
   echo "Test completed successfully!"
   echo "Pushing deploy commit..."
   git add . 2>&1> /dev/null
-  su - "$USER" -c "git commit -m 'Deploy commit $(date '+%Y%m%d-%H:%M:%S')'" 2>&1> /dev/null
+  git commit -m "Deploy commit $(date '+%Y%m%d-%H:%M:%S')" 2>&1> /dev/null
   git push 2>&1> /dev/null
   if [ $? -eq 0 ]; then
     echo "The main branch has been updated!"
@@ -35,14 +36,13 @@ if [ $result -eq 0 ]; then
   export ASPNETCORE_ENVIRONMENT=Production
   echo "Updating $ASPNETCORE_ENVIRONMENT environment..."
   # clear trash
-  sudo rm -r ~/delivery-api/*/bin
-  sudo rm -r ~/delivery-api/*/obj
-  su - "$USER" -c "dotnet ef database update --project ~/delivery-api/Delivery.Api" 2>&1> /dev/null
+  clear_build
+  dotnet ef database update --project ./Delivery.Api 2>&1> /dev/null
   # publish project to nginx folder
+  clear_build
   sudo dotnet publish -f net6.0 -o /var/www/delivery-api/ -c Release -r linux-x64 --self-contained false 2>&1> /dev/null
   # clear trash
-  sudo rm -r ~/delivery-api/*/bin
-  sudo rm -r ~/delivery-api/*/obj
+  clear_build
   echo "Restarting service..."
   # reload services
   sudo systemctl restart delivery-api.service 2>&1> /dev/null
